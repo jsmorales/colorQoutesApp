0f0b4e77c1e3fa606e03179b0b714af42765a20a
jsmorales <johan.morales@saiavh.com>
2018-11-07 15:45:55 -0500

    Se ve la explicacion de los ciclos de vida de las actividades, se mejora la app para que muestre mas colores e inicie con una cita aleatoria inicial.

--- app/src/main/java/com/example/johanmorales/colorquotes/MainActivity.java
+++ app/src/main/java/com/example/johanmorales/colorquotes/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends Activity {
     private TextView citaTextView, autorTextView;
     private Button nuevaCitaButton;
 
+    //primer estado del ciclo de vida de la actividad, viene con el bundle savedInstanceState
+    //con los estados guardados previamente
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //main method
@@ -33,6 +35,20 @@ public class MainActivity extends Activity {
         //starts a quote
         nuevaCitaButton.callOnClick();
 
+        //charge the estate on create method
+        /*
+        if(savedInstanceState != null){
+
+            citaTextView.setText(savedInstanceState.getString("cita"));
+            autorTextView.setText(savedInstanceState.getString("autor"));
+
+            int color = savedInstanceState.getInt("color");
+
+            citaTextView.setTextColor(color);
+            autorTextView.setTextColor(color);
+            nuevaCitaButton.setBackgroundColor(color);
+        }*/
+
         /*
         * Attach the click listener on the code
         * nuevaCitaButton.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +64,57 @@ public class MainActivity extends Activity {
             }
         });
         * */
+    }
+
+    //sigue el ciclo onStart, casi nunca se utiliza
+
+    //continua onResume se ejecuta cuando la actividad esta lista para el ususario
+    //se puede usar para inicializar animaciones o musica
+
+    //En este punrto la actividad se esta ejecutando en el top de la pila
+
+    //si el usuario sale se ejecuta
+    //OnPause() --> en este metodo se paran animaciones o musica y se pueden guardar datos en la bd
+    //OnStop() --> se llama cuando la actividad ya no es visible al usuario y queda pendiente para su destruccion
+    //OnDestroy() --> destruye la actividad por completo
+
+    //si una actividad esta OnStop() y se reanuda se ejecita el metodo OnRestart()
+    //la cual vuelve a ejecitar el metodo OnStart()
+
+    //para finalmente darla por terminada
+
+
+
+    //see this methods pressing ctrl+o
+
+    //for save the state of our main activity overrides the method:
+    //guarda estados
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        //Bundle is an object can save a key and a value
+        //on this case we save the cita, autor and the color
+        //"cita" deberia ser una constante como TAG --> CITA
+        outState.putString("cita",citaTextView.getText().toString());
+        outState.putString("autor",autorTextView.getText().toString());
+        outState.putInt("color", citaTextView.getCurrentTextColor());
+    }
+
+    //for restore the state saved previously
+    //carga estados
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        citaTextView.setText(savedInstanceState.getString("cita"));
+        autorTextView.setText(savedInstanceState.getString("autor"));
+
+        int color = savedInstanceState.getInt("color");
+
+        citaTextView.setTextColor(color);
+        autorTextView.setTextColor(color);
+        nuevaCitaButton.setBackgroundColor(color);
     }
 
     //Define the method for XML onClick Method
